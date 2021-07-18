d855092eae55a3bdfb16ee795ee2bf91aea76b20
Saac Sornchai <sornchai.la@ku.th>
2021-07-18 12:03:32 +0700

    add pokemon

--- src/store/Pokemon.js
+++ src/store/Pokemon.js
@@ -1,11 +1,12 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import Axios from "axios";
+import Vue from "vue"
+import Vuex from "vuex"
+import Axios from "axios"
 
-let api_endpoint =
-  "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
+// let api_endpoint =
+//   "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+let api_endpoint = "http://localhost:8080/pokedex.json"
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export const pokemon = new Vuex.Store({
   state: {
@@ -13,36 +14,36 @@ export const pokemon = new Vuex.Store({
   },
   mutations: {
     fetch(state, { res }) {
-      state.data = res.data;
+      state.data = res.data
     },
     add(state, { payload }) {
-      state.data.push(payload);
+      state.data.push(payload)
     },
     edit(state, { payload }) {
-      state.data[payload.index].name = payload.name;
-      state.data[payload.index].type = payload.type;
+      state.data[payload.index].name = payload.name
+      state.data[payload.index].type = payload.type
     },
     delete(state, { payload }) {
-      state.data.splice(payload.index, 1);
+      state.data.splice(payload.index, 1)
     },
   },
   actions: {
     async fetchPokemon({ commit }) {
-      let res = await Axios.get(api_endpoint);
-      commit("fetch", { res });
+      // let res = await Axios.get(api_endpoint)
+      // commit("fetch", { res })
     },
     addPokemon({ commit }, payload) {
-      commit("add", { payload });
+      commit("add", { payload })
     },
     deletePokemon({ commit }, payload) {
-      commit("delete", { payload });
+      commit("delete", { payload })
     },
     editPokemon({ commit }, payload) {
-      commit("edit", { payload });
+      commit("edit", { payload })
     },
   },
   modules: {},
   getters: {
     pokemons: (state) => state.data,
-  }
+  },
 })
--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,136 +1,23 @@
 <template>
-  <div class="container m-4">
+  <div class="container mx-auto">
     <h1 class="text-2xl mb-4">Pokemon List</h1>
-
-    <table class="min-w-full divide-y divide-gray-200">
-      <thead class="bg-pink-200">
-        <tr>
-          <th class="hidden md:inline-block px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">#</th>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Name</th>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Name (JP)</th>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Type</th>
-          <th class="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">Action</th>
-        </tr>
-      </thead>
-      <tbody class="divide-y divide-blue-200">
-        <tr class="hover:bg-yellow-300" v-for="(pokemon, index) in pokemons" :key="index">
-          <td class="hidden md:inline-block px-6 py-4 text-left whitespace-nowrap">
-            {{ index + 1 }}
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index !== editIndex">
-            {{ pokemon.name.english }}
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index !== editIndex">
-            {{ pokemon.name.japanese }}
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index !== editIndex">
-            {{ joinType(pokemon.type) }}
-          </td>
-          <td class="px-6 py-4 text-center whitespace-nowrap"
-            v-if="index !== editIndex">
-            <button class="px-2 bg-blue-100 rounded mx-2"
-              @click="openForm(index, pokemon)">
-              Edit
-            </button>
-            <button class="px-2 bg-red-200 rounded mx-2">
-              Delete
-            </button>
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index === editIndex">
-            <input type="text" 
-              class="border"
-              v-model="form.name.english">
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index === editIndex">
-            <input type="text" 
-              class="border"
-              v-model="form.name.japanese">
-          </td>
-          <td class="px-6 py-4 text-left whitespace-nowrap"
-            v-if="index === editIndex">
-            <input type="text" 
-              class="border"
-              v-model="form.type">
-          </td>
-          <td class="px-6 py-4 text-center whitespace-nowrap"
-            v-if="index === editIndex">
-            <button class="px-2 bg-blue-300 rounded mx-2"
-              @click="editPokemon(index, pokemon)">
-              Update
-            </button>
-            <button class="px-2 bg-yellow-200 rounded mx-2"
-              @click="closeForm()">
-              Cancel
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <pokemon-create-form></pokemon-create-form>
+    <pokemon-table></pokemon-table>
   </div>
 </template>
 
 <script>
-import { pokemon } from '@/store/Pokemon'
+import PokemonCreateForm from '@/components/PokemonCreateForm'
+import PokemonTable from "@/components/PokemonTable"
 
 export default {
-  data() {
-    return {
-      pokemons: [],
-      editIndex: -1,
-      form: {
-        name: {
-          english: '',
-          japanese: ''
-        },
-        type: []
-      }
-    }
+  components: {
+    PokemonTable,
+    PokemonCreateForm
   },
-  created() {
-    this.fetchPokemon()
-  },
-  methods: {
-    async fetchPokemon() {
-      await pokemon.dispatch('fetchPokemon')
-      this.pokemons = pokemon.getters.pokemons
-    },
-    openForm(index, pokemon) {
-      this.editIndex = index
-      this.form.name.english = pokemon.name.english
-      this.form.name.japanese = pokemon.name.japanese
-      this.form.type = this.joinType(pokemon.type)
-    },
-    closeForm() {
-      this.editIndex = -1;
-      this.form = {
-        name: {
-          english: '',
-          japanese: ''
-        },
-        type: []
-      }
-    },
-    async editPokemon() {
-      let payload = {
-        index: this.editIndex,
-        name: this.form.name,
-        type: this.form.type.split(',').map(item => item.trim())
-      }
-      await pokemon.dispatch('editPokemon', payload)
-      this.closeForm()
-    },
-    joinType(arr) {
-      return arr.join(', ')
-    }
-  }
+  created() {},
+  methods: {},
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
